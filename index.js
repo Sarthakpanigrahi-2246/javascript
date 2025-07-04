bb32a27241f55d6585243d7be7a03f5282af303a
sarthak panigrahi <sarthakpanigrahi2246@gmail.com>
2025-07-04 22:14:20 +0530

    operator and datatype

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log("hello hii");
-console.log("bfjerbfb");
+// let d=10;
+// let c=20;
+// console.log(d+c);
+
+// var c=10;
+// var c=20;
+// console.log(c+c)
+
+const adhar=22358010186;
+console.log(adhar);
+// let count = 10;
+// count--;
+// console.log(count);
+// let num=10 === 10;
+// console.log(num);
+let num = 1+'1'+1;
+console.log(typeof num); 
+
