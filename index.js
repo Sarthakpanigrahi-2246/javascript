2320cd73a84756c18c7e95dfd3e64d908253da33
sarthak panigrahi <sarthakpanigrahi2246@gmail.com>
2025-07-06 20:40:51 +0530

    if else and switch case

--- index.js
+++ index.js
@@ -6,13 +6,45 @@
 // var c=20;
 // console.log(c+c)
 
-const adhar=22358010186;
-console.log(adhar);
-// let count = 10;
-// count--;
-// console.log(count);
-// let num=10 === 10;
-// console.log(num);
-let num = 1+'1'+1;
-console.log(typeof num); 
+// const adhar=22358010186;
+// console.log(adhar);
+// // let count = 10;
+// // count--;
+// // console.log(count);
+// // let num=10 === 10;
+// // console.log(num);
+// let num = 1+'1'+1;
+// console.log(typeof num); 
 
+{let age= 10;
+// let check=age=>18;
+ if(age <=10)
+ {
+    console.log("bBY BOY"); 
+ }
+else if(age>=11 && age<=17)
+    {
+    console.log("little bit");
+}
+else  (age<=18 && age <60);
+{
+    console.log("adult");
+}}
+
+{let express= '-';
+let a=10;
+let b=20;
+
+switch (express)
+{
+    case '+' :console.log( a+b);
+    break;
+     case '-' :console.log( a-b);
+     break;
+      case '/' :console.log( a/b);
+       break;
+       case '*' :console.log( a*b);
+    break;
+    default : console.log("choose correct operator"); 
+
+}}
