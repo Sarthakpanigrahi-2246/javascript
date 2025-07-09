cea6f8cb0b3d0e77089394c8eb57ee8476a5ae5d
sarthak panigrahi <sarthakpanigrahi2246@gmail.com>
2025-07-09 20:27:14 +0530

    only string

--- index.js
+++ index.js
@@ -16,35 +16,72 @@
 // let num = 1+'1'+1;
 // console.log(typeof num); 
 
-{let age= 10;
-// let check=age=>18;
- if(age <=10)
- {
-    console.log("bBY BOY"); 
- }
-else if(age>=11 && age<=17)
-    {
-    console.log("little bit");
-}
-else  (age<=18 && age <60);
-{
-    console.log("adult");
-}}
+          //if else if else
+// {
+// let age= 10;
+// // let check=age=>18;
+//  if(age <=10)
+//  {
+//     console.log("bBY BOY"); 
+//  }
+// else if(age>=11 && age<=17)
+//     {
+//     console.log("little bit");
+// }
+// else  (age<=18 && age <60);
+// {
+//     console.log("adult");
+// }
+// }
 
-{let express= '-';
-let a=10;
-let b=20;
 
-switch (express)
-{
-    case '+' :console.log( a+b);
-    break;
-     case '-' :console.log( a-b);
-     break;
-      case '/' :console.log( a/b);
-       break;
-       case '*' :console.log( a*b);
-    break;
-    default : console.log("choose correct operator"); 
+// //       switch case 
+// {
+//     let express= '-';
+//     let a=10;
+//     let b=20;
 
-}}
+//     switch (express)
+//     {
+//     case '+' :console.log( a+b);
+//     break;
+//     case '-' :console.log( a-b);
+//     break;
+//     case '/' :console.log( a/b);
+//     break;
+//     case '*' :console.log( a*b);
+//     break;
+//     default : console.log("choose correct operator"); 
+//    }
+// }
+//  for(let i=0; i<=5; i++)
+//  {
+//     console.log("hello sp");
+//     console.log(i);
+//  }
+
+
+
+ // function and
+//  function sum(vala,valb,valc, name )
+//  {
+//    let add = vala+valb+valc;
+//    console.log(add, name)
+//    return add, name;
+//  }
+//    sum(10,40,30 , "aman");
+//    sum(10,40,30 , "om");
+//    sum(10,40,30 , "rohin");
+  //  console.log("aman m",add);
+   
+  let num=10.234324;
+   console.log(num.toPrecision(7));
+   console.log(typeof num)
+   let number = new Number(100);
+   console.log(typeof number);
+
+   console.log(number)
+
+   //string 
+   let str = new String("hello");
+   console.log(str)
